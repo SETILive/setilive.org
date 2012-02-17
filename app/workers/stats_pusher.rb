2a97492e01aee93d73068d9baf38d325c2f93d1c
Stuart Lynn <stuart.lynn@gmail.com>
2012-02-17 02:05:00 -0600

    stats pusher

---
+++ app/workers/stats_pusher.rb
@@ -0,0 +1,16 @@
+class StatsPusher
+  include Sidekiq::Worker 
+  
+  def perform
+    stats = {:total_classifications=>RedisConnection.get("total_classifications"),
+             :people_online=> RedisConnection.keys("online_*").count,
+             :total_users => ZooniverseUser.count,
+             :classification_rate => RedisConnection.keys("recent_classification_*").count
+           }
+
+    puts "pusing #{JSON.pretty_generate stats}"
+
+    Pusher['telescope'].trigger('stats_update', stats)
+
+  end
+end
