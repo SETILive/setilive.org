d30547a83a21b040259d33bcc3e7db611072477d
Stuart Lynn <stuart.lynn@gmail.com>
2012-02-17 00:01:43 -0600

    formatting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_filter CASClient::Frameworks::Rails::GatewayFilter
+  before_filter CASClient::Frameworks::Rails::GatewayFilter, :only =>[:index]
 
   def index 
     
@@ -9,17 +9,20 @@ class HomeController < ApplicationController
   def stats
     respond_to do |format|
       format.json do 
-        reply = Rails.cache.fetch(:stats, :expires_in => 2.seconds) do 
-          {:total_classifications=>RedisConnection.get("total_classifications"),
-           :people_online=> RedisConnection.keys("online_*").count,
-           :total_users => ZooniverseUser.count,
-           :classification_rate => RedisConnection.keys("recent_classification_*").count
-           }.to_json
-        end
+        reply = { total_classifications: RedisConnection.get("total_classifications"),
+                  people_online: RedisConnection.keys("online_*").count,
+                  total_users: ZooniverseUser.count,
+                  classification_rate: RedisConnection.keys("recent_classification_*").count }
         render :json=>reply
       end
     end
   end
 
-
+  def telescope_status
+     @status = RedisConnection.get('current_status')
+     respond_to do |format|
+       format.html
+       format.json{ render :json=> {status: @status}.to_json}
+     end
+  end
 end
