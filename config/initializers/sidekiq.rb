563c55ae65e97e6b0ac358b7317c05ef90ecd30e
Stuart Lynn <stuart.lynn@gmail.com>
2012-02-27 20:28:24 -0600

    proper selection of environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 redis_config = YAML.load_file('config/redis.yml')
-redis_config = redis_config['production'].inject({}){|r,a| r[a[0].to_sym]=a[1]; r}
+redis_config = redis_config[Rails.env].inject({}){|r,a| r[a[0].to_sym]=a[1]; r}
 
 
 Sidekiq::Client.redis = Sidekiq::RedisConnection.create(:url => "redis://#{redis_config['username']}:#{redis_config[:password]}@#{redis_config[:host]}:#{redis_config[:port]}/")
