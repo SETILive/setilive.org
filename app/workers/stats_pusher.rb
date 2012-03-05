f1755014ec70adb720cbe9d54d79a17b4fe28525
Michael Parrish <michael@zooniverse.org>
2012-03-05 11:15:49 -0600

    Fixing classification rate again

--- app/workers/stats_pusher.rb
+++ app/workers/stats_pusher.rb
@@ -5,12 +5,10 @@ class StatsPusher
     stats = {:total_classifications=>RedisConnection.get("total_classifications"),
              :people_online=> RedisConnection.keys("online_*").count,
              :total_users => RedisConnection.get('zooniverse_user_count'),
-             :classification_rate => RedisConnection.keys("recent_classification_*").count/10.0
+             :classification_rate => RedisConnection.keys("recent_classification_*").count
            }
-
+    
     # puts "pusing #{JSON.pretty_generate stats}"
-
     Pusher['telescope'].trigger('stats_update', stats)
-
   end
 end
