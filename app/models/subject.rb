4cf55fee8cd42040f5dc64f9eeae724900784a83
Stuart Lynn <stuart.lynn@gmail.com>
2012-06-27 01:26:59 -0500

    typo

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -269,7 +269,7 @@ class Subject
               throw "Could not find or create Source for observation #{beam['target_id']}"
             end
           end
-          s.observation.each {|o| o.processNow}
+          s.observations.each {|o| o.processNow}
         end
 
       rescue   Exception => e  
