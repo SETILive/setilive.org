dc3801f583190f5beb4f1ee5f46da09e02d688c3
Chris Snyder <bumishness@gmail.com>
2012-10-08 20:06:28 -0500

    Added two Marv endpoints for timers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,4 +31,20 @@ class HomeController < ApplicationController
        format.json{ render :json=> {status: @status}.to_json}
      end
   end
+
+  def time_to_followup
+     @time = RedisConnection.get('time_to_followup') || '0'
+     respond_to do |format|
+       format.html
+       format.json{ render :json=> {time: @time}.to_json}
+     end
+  end
+
+  def time_to_new_data
+     @time = RedisConnection.get('time_to_new_data') || '0'
+     respond_to do |format|
+       format.html
+       format.json{ render :json=> {status: @time}.to_json}
+     end
+  end
 end
