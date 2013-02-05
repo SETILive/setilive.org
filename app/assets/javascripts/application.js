6c7d6059533dd1d0356c9c16733bada7e0cefca0
Lou Nigra <lou@zooniverse.org>
2013-02-05 10:57:00 -0600

    Update countdown.js: Seems to fix non-refreshing timer issue on Chrome.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //= require ./jquery.svgdom.min.js
 //= require ./jquery.transform2d.js
 //= require ./date.js
-//= require ./countdown.js
+//= require ./jquery.countdown-1.6.1.js
 //= require app
